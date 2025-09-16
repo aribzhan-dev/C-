019b9d0f76fda19ae95d78abc1946de07e3a42ed
Aribzhan <aribzankamilzanov@gmail.com>
2025-09-16 15:54:19 +0500

    my home task and my first time coding on C#

---
+++ test.cs
@@ -0,0 +1,74 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("task 1");
+        // int year = 2025;
+        // Console.WriteLine($"Current year: {year}");
+        
+        // Console.WriteLine("task 2");
+        // string name = "Aribzhan";
+        // int age = 17;
+        // Console.WriteLine($"My name's {name} and i'm {age} yo");
+        
+        // Console.WriteLine("task 3");
+        // double price = 999.99;
+        // int quanity = 3;
+        // Console.WriteLine($"Sum : {price * quanity}");
+        
+        // Console.WriteLine("task 4");
+        // char letter = 'B';
+        // Console.WriteLine($"Choosen letter is {letter}");
+        
+        // Console.WriteLine("task 5");
+        // bool isRain = true;
+        // if (isRain == true)
+        //     Console.WriteLine("Take an  umbrella");
+        // else
+        //     Console.WriteLine("Wheather is clever");
+            
+        // Console.WriteLine("task 6");
+        // string city = "Astana";
+        // int polulation = 1300000;
+        // double area = 722.0;
+        // Console.WriteLine($"{city} has population: {polulation} and area: {area} km");
+        
+        // Console.WriteLine("task 7");
+        // int a = 7;
+        // int b = 3;
+        // double c = a / b; 
+        // Console.WriteLine($"Sum : {a+b}");
+        // Console.WriteLine($"Raznost: {a-b}");
+        // Console.WriteLine($"Delenie: {c}");
+        
+        
+        // Console.WriteLine("task 8");
+        // bool isAdult = false;
+        // if (isAdult == true)
+        //     Console.WriteLine("Access granted");
+        // else
+        //     Console.WriteLine("Access denied");
+            
+            
+        // Console.WriteLine("task 9");
+        // string name = Console.ReadLine();
+        // int age = Convert.ToInt32(Console.ReadLine());
+        // string city = Console.ReadLine();
+        // Console.WriteLine($"My name's {name} and i'm {age} and i live in {city}");
+        
+        Console.WriteLine("task 10");
+        Console.Write("Enter a number -->");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter a number -->");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"S = {a*b} and P = {(a+b)*2}");
+       
+        
+        
+    }
+}
